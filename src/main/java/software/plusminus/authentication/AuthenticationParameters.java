c914102e22ff044f8f1e48bba4e5d576a874b0a8
Taras Shpek <taras.shpek@gmail.com>
2021-09-08 01:28:03 +0300

    Refactor AuthenticationParameters

--- src/main/java/software/plusminus/authentication/AuthenticationParameters.java
+++ src/main/java/software/plusminus/authentication/AuthenticationParameters.java
@@ -1,16 +1,22 @@
 package software.plusminus.authentication;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-@Data
+@Getter
+@Builder
+@EqualsAndHashCode
 public class AuthenticationParameters {
 
     private String username;
+    @Builder.Default
     private Set<String> roles = Collections.emptySet();
+    @Builder.Default
     private Map<String, ?> otherParameters = Collections.emptyMap();
     
 }
