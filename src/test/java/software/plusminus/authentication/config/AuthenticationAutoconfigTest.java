d052422444bc306c390cf52e813aa6ea01df55e2
Taras Shpek <taras.shpek@gmail.com>
2022-07-26 20:33:59 +0300

    add tests

---
+++ src/test/java/software/plusminus/authentication/config/AuthenticationAutoconfigTest.java
@@ -0,0 +1,27 @@
+package software.plusminus.authentication.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import software.plusminus.authentication.service.AuthenticationService;
+
+import static software.plusminus.check.Checks.check;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AuthenticationAutoconfig.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class AuthenticationAutoconfigTest {
+    
+    @Autowired(required = false)
+    private AuthenticationService authenticationService;
+    
+    @Test
+    public void defaultAuthenticationServiceIsPresent() {
+        check(authenticationService).isNotNull();
+    }
+}
