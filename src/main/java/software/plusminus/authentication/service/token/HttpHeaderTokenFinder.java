625f15e57816e29c0d13d9a93eddf605f9cf1688
Taras Shpek <taras.shpek@gmail.com>
2025-11-20 20:12:51 +0200

    Return token-related code

--- src/main/java/software/plusminus/authentication/service/token/HttpHeaderTokenFinder.java
+++ src/main/java/software/plusminus/authentication/service/token/HttpHeaderTokenFinder.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import software.plusminus.context.Context;
-import software.plusminus.security.service.token.TokenFinder;
 
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
